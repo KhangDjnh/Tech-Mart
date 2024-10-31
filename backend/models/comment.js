861bedf441b82fd8608c6a32c6fa7fe586c2a56f
ndk-218 <khoatink99lhp@gmail.com>
2024-10-31 18:04:29 +0700

    4 models cuoi

---
+++ backend/models/comment.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const CommentSchema = new mongoose.Schema({
+    id_user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',  // Tham chiếu đến model User (chủ sở hữu cmt)
+        required: true 
+    },
+    id_product: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Product',  // Tham chiếu đến model Product (sản phẩm của cmt)
+        required: true 
+    },
+    content: { 
+        type: String, 
+        required: true,
+        trim: true,
+        maxlength: 500  // Giới hạn nội dung bình luận tối đa 500 ký tự
+    },
+    rating: { 
+        type: Number, 
+        default: 0,
+        min: 0,  // Giá trị tối thiểu của đánh giá là 0
+        max: 5   // Giá trị tối đa của đánh giá là 5
+    },
+}, { timestamps: true }); 
+
+module.exports = mongoose.model('Comment', CommentSchema);
---
+++ backend/models/order.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+    id_user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',  // Tham chiếu đến model User (chủ sở hữu đơn hàng)
+        required: true 
+    },
+    id_product: [{ 
+        product: {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'Product',  // Tham chiếu đến model Product (sản phẩm trong đơn hàng)
+            required: true 
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            min: 1,  // Số lượng tối thiểu là 1
+            max: 10  // Số lượng tối đa là 10
+        }
+    }],
+    total_price: {
+        type: Number,
+        required: true,
+        min: 0  // Giá trị tối thiểu là 0 để tránh giá trị âm
+    },
+    status: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100,
+        enum: ['pending', 'confirmed', 'shipped', 'delivered', 'canceled']  // Các trạng thái hợp lệ cho đơn hàng
+    },
+    address: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100
+    }
+}, { timestamps: true }); 
+
+module.exports = mongoose.model('Order', OrderSchema);
