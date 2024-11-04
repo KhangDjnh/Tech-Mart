ed1bb7a6c00d3dc114e5150efb7d1167ac2a9a22
KhangDjnh <khangdjnh24042002@gmail.com>
2024-11-04 16:56:22 +0700

    Create module Register, Login

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,24 +1,17 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true, unique: true },       
+    password: { type: String, required: true, minlength: 8, maxlength: 1024},   
+    email: { type: String, required: true, unique: true },
+    emailConfirmed: {type: Boolean, default: flase},        
     phonenumber: { type: String, required: true },              
     gender: { type: String, enum: ['Male', 'Female', 'Other'] },  
     birthday: { type: Date },                           
     role: { type: String, enum: ['buyer', 'seller', 'admin'], default: 'buyer' }, 
     profilePic: { type: String },                     
-    coverPic: { type: String },                  
+    coverPic: { type: String },                     
     id_following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Follow' }],
 }, { timestamps: true });
-
-// Hàm mã hóa mật khẩu trước khi lưu
-UserSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-});
 
 module.exports = mongoose.model('User', UserSchema);
