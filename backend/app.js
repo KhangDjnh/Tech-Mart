fa49c3029a9592003ee74667bb4faf46d156a07f
email <khanhtoshiro2003@gmail.com>
2024-11-22 23:58:13 +0700

    add router to app

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,21 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const http = require("http");
 
+const socketServer = require("./utils/serverSocket");
+
 const userRouter = require("./routes/UserRouter");
 const registerRouter = require("./routes/RegisterRouter");
 const loginRouter = require("./routes/LoginRouter");
 const confirmRouter = require("./routes/ConfirmRouter");
-const socketServer = require("./utils/serverSocket");
 const forgotPassRouter = require("./routes/ForgotPassRouter");
-
+const cartRouter = require("./routes/CartRouter");
+const commentRouter = require("./routes/CommentRouter");
+const conversationRouter = require("./routes/ConversationRouter");
+const messageRouter = require("./routes/MessageRouter");
+const orderRouter = require("./routes/OrderRouter");
+const productRouter = require("./routes/ProductRouter");
+const shopRouter = require("./routes/ShopRouter");
+const tagRouter = require("./routes/TagRouter");
 
 require("dotenv").config();
 
@@ -45,5 +53,14 @@ app.use("/api/register", registerRouter);
 app.use("/api/login", loginRouter);
 app.use("/api/forgot_pass", forgotPassRouter);
 app.use("/api/confirm", confirmRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/comment", commentRouter);
+app.use("/api/conversation", conversationRouter);
+app.use("/api/message", messageRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/product", productRouter);
+app.use("/api/shop", shopRouter);
+app.use("/api/tag", tagRouter);
+
 
 module.exports = app;
