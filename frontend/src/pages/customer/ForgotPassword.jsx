95ac93adc9ee51d216d2362428bfcff09a0d557f
email <khanhtoshiro2003@gmail.com>
2024-12-04 01:41:59 +0700

    update forgotpass  box

--- frontend/src/pages/customer/ForgotPassword.jsx
+++ frontend/src/pages/customer/ForgotPassword.jsx
@@ -8,15 +8,15 @@ import {
     InputLabel,
     OutlinedInput,
 } from "@mui/material";
-import {Visibility, VisibilityOff} from "@mui/icons-material";
-import {useEffect, useState} from "react";
-import {Link, useNavigate} from "react-router-dom";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import eventEmitter from "../../utils/eventEmitter.js";
-import {notify} from "../../utils/toastify.js";
-import {userApi} from "../../../api/userApi.js";
+import { notify } from "../../utils/toastify.js";
+import { userApi } from "../../../api/userApi.js";
 
 
-export default function ForgotPassword({register}) {
+export default function ForgotPassword({ register }) {
 
     const navigate = useNavigate();
     const [send, setSend] = useState(false)
@@ -47,12 +47,16 @@ export default function ForgotPassword({register}) {
                 <div className="px-10  rounded-md  flex flex-col justify-center items-center w-full  mt-10">
                     <h1 className="font-medium text-xl mb-6 mt-3">Quên mật khẩu</h1>
                     <div className={"grid grid-cols-[80%,20%] gap-3 w-full"}>
-                        <TextField
-                            label="Email"
-                            className="w-full !my-4"
-                            value={email}
-                            onChange={handleEmailChange}
-                        />
+                        <FormControl variant="outlined" className={"w-full !my-4"}>
+                            <InputLabel htmlFor="outlined-adornment-email">Email</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-email"
+                                value={email}
+                                onChange={handleEmailChange}
+                                type="email"
+                                label="Email"
+                            />
+                        </FormControl>
                         <Button
                             variant="contained"
                             className={"!my-4"}
@@ -63,7 +67,7 @@ export default function ForgotPassword({register}) {
                         </Button>
                     </div>
                     <div className={'flex justify-end items-center w-full p-6'}><Link to={'/login'}
-                                                                                      className={'underline hover:text-blue-700 hover:cursor-pointer'}>Trở
+                        className={'underline hover:text-blue-700 hover:cursor-pointer'}>Trở
                         về trang đăng nhập</Link></div>
                 </div>
             </div>
