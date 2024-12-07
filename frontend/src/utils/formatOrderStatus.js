310b847f56d1a8b5f70852d871b31b921e03ad36
LittleSlimeBoi <97430502+LittleSlimeBoi@users.noreply.github.com>
2024-12-08 00:01:36 +0700

    util

--- frontend/src/utils/formatOrderStatus.js
+++ frontend/src/utils/formatOrderStatus.js
@@ -1,8 +1,8 @@
 export function formatStatus(status){
     if(status == "pending") return "Đang chờ xác nhận";
     else if(status == "confirmed") return "Đã xác nhận";
-    else if(status == "confirmed") return "Đã vận chuyển";
-    else if(status == "confirmed") return "Đã giao hàng";
-    else if(status == "confirmed") return "Đơn hủy";
+    else if(status == "shipped") return "Đand vận chuyển";
+    else if(status == "delivered") return "Đã giao hàng";
+    else if(status == "canceled") return "Đơn hủy";
     else return "Đơn lỗi";
 }
