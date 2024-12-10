502d38cd6bb60274a3f1c9139dc0b114dfc3a6c6
email <khanhtoshiro2003@gmail.com>
2024-12-11 02:47:36 +0700

    drop page

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -44,7 +44,8 @@ exports.getAllProducts = async (req, res) => {
         // const page = parseInt(req.query.page) || 1; 
         // const limit = parseInt(req.query.limit) || 12;
 
-        const products = await productService.getAllProducts(page, limit);
+        // const products = await productService.getAllProducts(page, limit);
+        const products = await productService.getAllProducts();
         res.status(200).json({ data: products, status: "success", page, limit });
     } catch (err) {
         res.status(500).json({ error: err.message });
